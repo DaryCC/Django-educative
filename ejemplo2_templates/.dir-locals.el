3f03c026df874f772d41aa33e674a26c49b63cbf
dcc <dcc@pop-os.localdomain>
2022-03-10 01:59:41 -0600

    Relative-URL tags and loops. conditionals, filters

---
+++ ejemplo2_templates/.dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+;; ((python-mode . ((python-shell-interpreter . "/usr/bin/python3"))))
+((nil
+  (pony-settings .
+                 (make-pony-project
+                  :python "/usr/bin/python3"
+                  :settings )
+                 )
+  ))
